e2c0c76872627a0ecd3ed8f1533acb0b94dcd11d
dirha1996 <edi.nugraha404@gmail.com>
2023-03-27 15:02:39 +0700

    Initial commit

---
+++ application/views/pages/about/index.php
@@ -0,0 +1,26 @@
+<!-- ============================================================== -->
+<!-- Container fluid  -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+
+    <?php $this->load->view('layouts/_alert') ?>
+
+    <div class="card-group">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-fluid" height="890" width="593" src="<?= base_url('assets/images/about/logo.png') ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">Edi Nugraha</h4>
+                        <p class="card-text">043856583</p>
+                        <a href="https://github.com/dirha1996" class="btn btn-primary">Github Account</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid  -->
+<!-- ============================================================== -->
